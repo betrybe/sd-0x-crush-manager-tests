c609ef305da28dd06939a76227be1971ea17c2f2
Renato Filho <renatosousafilho@gmail.com>
2021-02-07 22:20:54 -0300

    Refatorando testes dos requisitos 1 e 2

--- tests/getAllCrushs.test.js
+++ tests/getAllCrushs.test.js
@@ -9,73 +9,35 @@ describe('1 - Crie o endpoint GET /crush', () => {
   beforeEach(() => {
     const crushSeed = fs.readFileSync(
       path.join(__dirname, 'seed.json'),
-      'utf8'
+      'utf8',
     );
 
     fs.writeFileSync(
       path.join(__dirname, '..', 'crush.json'),
       crushSeed,
-      'utf8'
+      'utf8',
     );
   });
 
-  it('Será validado que o endpoint deve retornar um array com todos os crushs cadastrados', async () => {
+  it('Será validado que o endpoint retorna um array com todos os crushs cadastrados', async () => {
     await frisby
-      .post(`${url}/login`, {
-        body: {
-          email: 'deferiascomigo@gmail.com',
-          password: '12345678',
-        },
-      })
-      .then((response) => {
-        const { body } = response;
-        const result = JSON.parse(body);
-        return frisby
-          .setup({
-            request: {
-              headers: {
-                Authorization: result.token,
-                'Content-Type': 'application/json',
-              },
-            },
-          })
-          .get(`${url}/crush`)
-          .expect('status', 200)
-          .then((response) => {
-            const { json } = response;
-            expect(json).toEqual(crushsSeed);
-          });
+      .get(`${url}/crush`)
+      .expect('status', 200)
+      .then((responseGet) => {
+        const { json } = responseGet;
+        expect(json).toEqual(crushsSeed);
       });
   });
 
-  it('Será validado que o endpoint deve retornar um array vazio caso não haja crushs', async () => {
+  it('Será validado que o endpoint retorna um array vazio caso não haja crushs cadastrados', async () => {
     fs.writeFileSync(path.join(__dirname, '..', 'crush.json'), '[]', 'utf8');
 
     await frisby
-      .post(`${url}/login`, {
-        body: {
-          email: 'deferiascomigo@gmail.com',
-          password: '12345678',
-        },
-      })
-      .then((response) => {
-        const { body } = response;
-        const result = JSON.parse(body);
-        return frisby
-          .setup({
-            request: {
-              headers: {
-                Authorization: result.token,
-                'Content-Type': 'application/json',
-              },
-            },
-          })
-          .get(`${url}/crush`)
-          .expect('status', 200)
-          .then((responseGet) => {
-            const { json } = responseGet;
-            expect(json).toEqual([]);
-          });
+      .get(`${url}/crush`)
+      .expect('status', 200)
+      .then((responseGet) => {
+        const { json } = responseGet;
+        expect(json).toEqual([]);
       });
   });
 });
--- tests/getCrushById.test.js
+++ tests/getCrushById.test.js
@@ -6,42 +6,40 @@ const url = 'http://localhost:3000';
 
 describe('2 - Crie o endpoint GET /crush/:id', () => {
   beforeEach(() => {
-    const crushSeed = fs.readFileSync(path.join(__dirname, 'seed.json'), 'utf8');
-  
-    fs.writeFileSync(path.join(__dirname, '..', 'crush.json'), crushSeed, 'utf8');
+    const crushSeed = fs.readFileSync(
+      path.join(__dirname, 'seed.json'),
+      'utf8',
+    );
+
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'crush.json'),
+      crushSeed,
+      'utf8',
+    );
   });
 
-  it('Será validado que o endpoint deve retornar um crush baseado no id da rota. Devendo retornar o `status 200` ', async () => {
+  it('Será validado que o endpoint retorna um crush baseado no id da rota', async () => {
     await frisby
-      .post(`${url}/login`, {
-        body: {
-          email: 'deferiascomigo@gmail.com',
-          password: '12345678',
-        },
-      })
-      .then((response) =>{
-        const { body } = response;
-        const result = JSON.parse(body);
-        return frisby
-          .setup({
-            request: {
-              headers: {
-                Authorization: result.token,
-                'Content-Type': 'application/json',
-              },
-            },
-          })
-          .get(`${url}/crush/1`)
-          .expect('status', 200)
-          .then((responseGet) => {
-            const { json } = responseGet;
-            expect(json).toEqual({
-              "name": "Madonna",
-              "age": 62,
-              "id": 1,
-              "date": { "datedAt": "23/10/2020", "rate": 5 }
-            })
-          })
+      .get(`${url}/crush/1`)
+      .expect('status', 200)
+      .then((responseGet) => {
+        const { json } = responseGet;
+        expect(json).toEqual({
+          name: 'Madonna',
+          age: 62,
+          id: 1,
+          date: { datedAt: '23/10/2020', rate: 5 },
         });
-    })
+      });
+  });
+
+  it('Será validado que o endpoint retonar um erro caso nenhum crush seja encontrado', async () => {
+    await frisby
+      .get(`${url}/crush/9`)
+      .expect('status', 404)
+      .then((responseGet) => {
+        const { json } = responseGet;
+        expect(json.message).toBe('Crush não encontrado');
+      });
+  });
 });
